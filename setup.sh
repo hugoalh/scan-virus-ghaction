e063ef1282d94176005637c2c541b21ebf365021
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-12-10 11:38:40 +0800

    20211210B

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
-mkdir --parents /opt/microsoft/powershell/7
-tar zxf /tmp/powershell-7.2.0-linux-alpine-x64.tar.gz -C /opt/microsoft/powershell/7 -v
-rm --force /tmp/powershell-7.2.0-linux-alpine-x64.tar.gz
-ln --symbolic /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+# $<Syntax>$ https://busybox.net/downloads/BusyBox.html
+echo "Setup PowerShell."
+mkdir -p /opt/microsoft/powershell/7
+tar zxf /tmp/powershell-7.2.0-linux-alpine-x64.tar.gz -C /opt/microsoft/powershell/7
+rm -f /tmp/powershell-7.2.0-linux-alpine-x64.tar.gz
+ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 chmod a+x,o-w /opt/microsoft/powershell/7/pwsh
+echo "Setup main system."
 apk update
 apk upgrade
 apk add --allow-untrusted --no-cache ca-certificates clamav@edgecommunity clamav-clamdscan@edgecommunity clamav-daemon@edgecommunity clamav-db@edgecommunity clamav-doc@edgecommunity clamav-libs@edgecommunity clamav-libunrar@edgecommunity clamav-milter@edgecommunity clamav-scanner@edgecommunity curl freshclam@edgecommunity git@edge icu-libs krb5-libs less libgcc libintl libssl1.1 libstdc++ lttng-ust@edge ncurses-terminfo-base openssh-client@edge tzdata userspace-rcu zlib
