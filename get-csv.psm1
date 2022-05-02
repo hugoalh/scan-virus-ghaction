fbd3b12522161336592287aa5d28b7f23219aa09
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-05-02 18:36:52 +0800

    Fix type casting

--- get-csv.psm1
+++ get-csv.psm1
@@ -3,7 +3,7 @@ function Get-Csv {
 	param (
 		[Parameter(Mandatory = $true, Position = 0)][string]$Path,
 		[string]$Delimiter = ',',
-		[System.Text.Encoding]$Encoding = 'UTF8NoBOM'
+		[ValidateSet('ASCII', 'BigEndianUnicode', 'BigEndianUTF32', 'OEM', 'Unicode', 'UTF7', 'UTF8', 'UTF8BOM', 'UTF8NoBOM', 'UTF32')][string]$Encoding = 'UTF8NoBOM'
 	)
 	[string[]]$Raw = Get-Content -Path $Path -Encoding $Encoding
 	return ConvertFrom-Csv -InputObject $Raw[1..(($Raw.Count -gt 1) ? ($Raw.Count - 1) : 1)] -Delimiter $Delimiter -Header ($Raw[0] -split $Delimiter)
