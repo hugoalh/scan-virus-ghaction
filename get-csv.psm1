d28c7397a3d577b626d4ef4869280532d73dd377
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-05-13 17:43:14 +0800

    20220513A

--- get-csv.psm1
+++ get-csv.psm1
@@ -1,11 +1,11 @@
 function Get-Csv {
 	[CmdletBinding()][OutputType([pscustomobject[]])]
 	param (
-		[Parameter(Mandatory = $true, Position = 0)][string]$Path,
-		[string]$Delimiter = ',',
+		[Parameter(Mandatory = $true, Position = 0)][Alias('LP', 'PSPath')][string]$LiteralPath,
+		[char]$Delimiter = ',',
 		[ValidateSet('ASCII', 'BigEndianUnicode', 'BigEndianUTF32', 'OEM', 'Unicode', 'UTF7', 'UTF8', 'UTF8BOM', 'UTF8NoBOM', 'UTF32')][string]$Encoding = 'UTF8NoBOM'
 	)
-	[string[]]$Raw = Get-Content -Path $Path -Encoding $Encoding
+	[string[]]$Raw = Get-Content -LiteralPath $LiteralPath -Encoding $Encoding
 	return ConvertFrom-Csv -InputObject $Raw[1..(($Raw.Count -gt 1) ? ($Raw.Count - 1) : 1)] -Delimiter $Delimiter -Header ($Raw[0] -split $Delimiter)
 }
 Export-ModuleMember -Function 'Get-Csv'
--- yara-rules/yara-community/utils/yara-forensics/file/dex.yar
+++
@@ -1,7 +0,0 @@
-rule dex : DEX Dalvik
-{
-    meta:
-        reference = "https://source.android.com/devices/tech/dalvik/dex-format#dex-file-magic"
-    condition:
-        uint32(0) == 0x0a786564
-}
