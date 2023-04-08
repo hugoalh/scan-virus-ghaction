9e2443388feec2bb26705d915ef435b7cd059918
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-04-08 17:13:54 +0800

    20230408F

--- lib/debug.ps1
+++ lib/debug.ps1
@@ -1,6 +1,4 @@
 #!/usr/bin/env pwsh
 #Requires -PSEdition Core -Version 7.3
 $Script:ErrorActionPreference = 'Stop'
-Import-Module -Name 'hugoalh.GitHubActionsToolkit' -Scope 'Local'
-Test-GitHubActionsEnvironment -Mandatory
-Write-GitHubActionsNotice -Message 'Hello, world!'
+Write-Host -Object 'Hello, world!'
